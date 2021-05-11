f61424a959f28fec9ca0eafeacc05c4554b02030
hallebanis <70589752+hallebanis@users.noreply.github.com>
2021-05-11 11:49:54 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,67 +1,79 @@
-
-import 'bootstrap/dist/css/bootstrap.min.css'
-import React from 'react'
-import './App.css';
-import {v4 as uuidv4} from "uuid"
-import ListMovies from "./components/ListMovies"
-import AddModal from './components/AddModal';
-
-
-
-
-
+import "bootstrap/dist/css/bootstrap.min.css";
+import React from "react";
+import "./App.css";
+import { v4 as uuidv4 } from "uuid";
+import ListMovies from "./components/ListMovies";
+import AddModal from "./components/AddModal";
 
 class App extends React.Component {
   state = {
-    movies : [{
-      id : uuidv4(),
-      title : "the invisible guest",
-      description : "The Invisible Guest (Spanish: Contratiempo, lit. 'Setback') is a Spanish mystery thriller film written and directed by Oriol Paulo. It was released in Spain on 6 January 2017. The film opened to lukewarm critical response, but was a commercial success, grossing $30.5 million against its €4 million budget.",
-      posterUrl : "https://th.bing.com/th/id/R51ce75bb09991f36bcbc49691529dc3b?rik=PjuK2L8VUZlMMw&pid=ImgRaw",
-      rating : 5,
-      year : "2016"
-    },
-    {
-      id : uuidv4(),
-      title : "Your Name.",
-      description : "Mitsuha and Taki are total strangers living completely different lives. But when Mitsuha makes a wish to leave her mountain town for the bustling city of Tokyo, they become connected in a bizarre way. She dreams she is a boy living in Tokyo while Taki dreams he is a girl from a rural town he’s never visited.",
-      posterUrl : "https://static.cinebel.be/img/movie/poster/full/1017364_fr_your_name_1511451802582.jpg",
-      rating : 4 ,
-      year : "2017"
-    },
-    {
-      id : uuidv4(),
-      title : "Social Dilemma",
-      description : "The Social Dilemma is an American docudrama film directed by Jeff Orlowski and written by Orlowski, Davis Coombe, and Vickie Curtis. It goes into depth on how social media's design is meant to nurture an addiction, manipulate people and governments, and spread conspiracy theories and disinformation.",
-      posterUrl : "https://th.bing.com/th/id/R0c89ffd52457bf5ca1f3bb1fc65d4b59?rik=OvEZ3CW8Db%2fKcw&pid=ImgRaw",
-      rating : 3 ,
-      year : "2020"
-    },
-    {
-      id : uuidv4(),
-      title : "Soul",
-      description : "Produced by Pixar and released by Disney , the story follows a middle school music teacher named Joe Gardner, who seeks to reunite his soul and his body after they are accidentally separated, just before his big break as a jazz musician. Soul is the first Pixar film to feature an African-American protagonist.",
-      posterUrl : "https://th.bing.com/th/id/OIP.bRMEBwz-3oMpbc6P-GiOZQHaLH?pid=ImgDet&rs=1",
-      rating : 4 ,
-      year : "2020"
-    }
-  ]
-  }
+    movies: [
+      {
+        id: uuidv4(),
+        title: "the invisible guest",
+        description:
+          "The Invisible Guest (Spanish: Contratiempo, lit. 'Setback') is a Spanish mystery thriller film written and directed by Oriol Paulo. It was released in Spain on 6 January 2017. The film opened to lukewarm critical response, but was a commercial success, grossing $30.5 million against its €4 million budget.",
+        posterUrl:
+          "https://th.bing.com/th/id/R51ce75bb09991f36bcbc49691529dc3b?rik=PjuK2L8VUZlMMw&pid=ImgRaw",
+        rating: 5,
+        year: "2016"
+      },
+      {
+        id: uuidv4(),
+        title: "Your Name.",
+        description:
+          "Mitsuha and Taki are total strangers living completely different lives. But when Mitsuha makes a wish to leave her mountain town for the bustling city of Tokyo, they become connected in a bizarre way. She dreams she is a boy living in Tokyo while Taki dreams he is a girl from a rural town he’s never visited.",
+        posterUrl:
+          "https://static.cinebel.be/img/movie/poster/full/1017364_fr_your_name_1511451802582.jpg",
+        rating: 4,
+        year: "2017"
+      },
+      {
+        id: uuidv4(),
+        title: "Social Dilemma",
+        description:
+          "The Social Dilemma is an American docudrama film directed by Jeff Orlowski and written by Orlowski, Davis Coombe, and Vickie Curtis. It goes into depth on how social media's design is meant to nurture an addiction, manipulate people and governments, and spread conspiracy theories and disinformation.",
+        posterUrl:
+          "https://th.bing.com/th/id/R0c89ffd52457bf5ca1f3bb1fc65d4b59?rik=OvEZ3CW8Db%2fKcw&pid=ImgRaw",
+        rating: 3,
+        year: "2020"
+      },
+      {
+        id: uuidv4(),
+        title: "Soul",
+        description:
+          "Produced by Pixar and released by Disney , the story follows a middle school music teacher named Joe Gardner, who seeks to reunite his soul and his body after they are accidentally separated, just before his big break as a jazz musician. Soul is the first Pixar film to feature an African-American protagonist.",
+        posterUrl:
+          "https://th.bing.com/th/id/OIP.bRMEBwz-3oMpbc6P-GiOZQHaLH?pid=ImgDet&rs=1",
+        rating: 4,
+        year: "2020"
+      }
+    ]
+  };
+  addMovie = (newMovie) => {
+    this.setState({
+      ...this.state,
+      movies: [...this.state.movies, newMovie]
+    });
+  };
 
-  render () {
-
+  render() {
     return (
-    <div className="App">
-      <header className="App-header">
-        <h1> <span>Saturday Nights </span> Movies </h1>
+      <div className="App">
+        <header className="App-header">
+          <h1>
+            {" "}
+            <span>Saturday Nights </span> Movies{" "}
+          </h1>
+        </header>
 
-      </header>
-      <ListMovies movies={this.state.movies}/> 
-      <AddModal />
-    </div>
-  );
-}
+        <ListMovies
+          movies={this.state.movies.filter((el) => el.title.includes("a"))}
+        />
+        <AddModal addMovie={this.addMovie} />
+      </div>
+    );
+  }
 }
 
 export default App;
-
--- src/components/AddModal.js
+++ src/components/AddModal.js
@@ -1,75 +1,99 @@
-import Modal from 'react-bootstrap/Modal'
-import Button from 'react-bootstrap/Button'
-import React from 'react'
-import 'bootstrap/dist/css/bootstrap.min.css';
-import AddMovie from './AddMovie';
-
-
-
-
+import { Modal } from "react-bootstrap";
+import Button from "react-bootstrap/Button";
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+//import AddMovie from "./AddMovie";
+import { v4 as uuidv4 } from "uuid";
 
 class AddModal extends React.Component {
-    constructor (props){
-        super(props);
-        this.state = {
-            show : false,
-        };
-    }
-    
-    handleShow = () => {
-        this.setState ({ show : !this.state.show}) ;
+  constructor(props) {
+    super(props);
+    this.state = {
+      show: false,
+      newMovie: {
+        id: uuidv4(),
+        title: "",
+        description: "",
+        posterUrl: "",
+        rating: 1,
+        year: ""
+      }
     };
-    handleChange =(event) => {
-        this.setState({
-            [event.target.name] : event.target.value 
-            
-        })
-    }
-    
-    render () {
-        return ( 
-            <>
-            <Button variant="primary" onClick={this.handleShow}>+</Button>
-            <Modal show ={this.state.show} onHide={this.handleShow}
-             size="lg"
-             aria-labelledby="contained-modal-title-vcenter"
-             centered 
-             style ={{ textAlign : "center"}}>
-                <Modal.Header closeButton > 
-                    <Modal.Title> You have some other Suggestions? Add Here : </Modal.Title>
-                </Modal.Header>
-                <Modal.Body>
-                    <div>
-                        <div>
-                            <label className ="input"> Title :</label>
-                            <input type ="text" name ="title" onChange={this.handleChange}/>
-                        </div>
-                        <div>
-                            <label className ="input"> Description : :</label>
-                            <input type ="text" name ="description" onChange={this.handleChange}/>
-                        </div>
-                        <div>
-                            <label className ="input"> Poster :</label>
-                            <input type ="text" name ="posterUrl" alt='' onChange={this.handleChange} />
-                        </div>
-                        <div>
-                            <label className ="input"> Rate :</label>
-                            <input type ="text" name ="rating" onChange={this.handleChange} />
-                        </div>
-                        <div>
-                            <label className ="input"> Year of Release :</label>
-                            <input type ="text" name ="year"  onChange={this.handleChange} />
-                        </div>
-                        </div>
-        </Modal.Body>
-       <Button onClick={AddMovie}>
-           Submit
-       </Button>
+  }
+
+  handleShow = () => {
+    this.setState({ show: !this.state.show });
+  };
+  handleChange = (event) => {
+    this.setState({
+      ...this.state,
+      newMovie: {
+        ...this.state.newMovie,
+        [event.target.name]: event.target.value
+      }
+    });
+  };
+
+  render() {
+    return (
+      <>
+        <Button variant="primary" onClick={this.handleShow}>
+          +
+        </Button>
+        <Modal
+          show={this.state.show}
+          onHide={this.handleShow}
+          size="lg"
+          aria-labelledby="contained-modal-title-vcenter"
+          centered
+          style={{ textAlign: "center" }}
+        >
+          <Modal.Header closeButton>
+            <Modal.Title>
+              {" "}
+              You have some other Suggestions? Add Here :{" "}
+            </Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <div>
+              <div>
+                <label className="input"> Title :</label>
+                <input type="text" name="title" onChange={this.handleChange} />
+              </div>
+              <div>
+                <label className="input"> Description : :</label>
+                <input
+                  type="text"
+                  name="description"
+                  onChange={this.handleChange}
+                />
+              </div>
+              <div>
+                <label className="input"> Poster :</label>
+                <input
+                  type="text"
+                  name="posterUrl"
+                  alt=""
+                  onChange={this.handleChange}
+                />
+              </div>
+              <div>
+                <label className="input"> Rate :</label>
+                <input type="text" name="rating" onChange={this.handleChange} />
+              </div>
+              <div>
+                <label className="input"> Year of Release :</label>
+                <input type="text" name="year" onChange={this.handleChange} />
+              </div>
+            </div>
+          </Modal.Body>
+          <Button onClick={() => this.props.addMovie(this.state.newMovie)}>
+            Submit
+          </Button>
         </Modal>
-            </>
-        )
-    }
+      </>
+    );
+  }
 }
 
-
-export default AddModal ;
+export default AddModal;
